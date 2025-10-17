5f87d1f389caaa2bbfb6c9eefc7d16a49c5229e6
Muhammad Saad <hafizmuhammadsaadshahid786@gmail.com>
2025-10-17 19:26:17 +0500

    Updated index.html

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
         <div className="container-fluid">
-          <Link className="navbar-brand web-name" to="#">H&S FUTURE AI</Link>
+          <Link className="navbar-brand web-name" to="#">Robot Shop</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
